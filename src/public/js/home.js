5e73620e1a0dd7de0c20c5f63dae2839dfad01f7
Oberdan <oberdan@craos.net>
2020-07-22 11:50:36 -0300

    Informações da unidade

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -5,6 +5,11 @@ constructor(params) {
 
     this.AbrirRecurso('html/home.html').then(value => {
         params.page.innerHTML = value.toString();
+        
+        document.getElementById('responsavel').innerText = params.unidade.responsavel;
+        document.getElementById('bloco').innerText = params.unidade.bloco;
+        document.getElementById('unidade').innerText = params.unidade.unidade;
+
         window.dispatchEvent(new CustomEvent('AoCaregarHome', {}));
     });
 }
