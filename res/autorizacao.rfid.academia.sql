61823231da08d2aa0715453d70b71f68360df5b7
Oberdan <oberdan@craos.net>
2020-08-13 15:37:30 -0300

    Adiciona a verificação de chaveiros do monitoramento

--- res/autorizacao.rfid.academia.sql
+++ res/autorizacao.rfid.academia.sql
@@ -1,5 +1,4 @@
-create or replace function acesso.autorizacao_rfid_webservice_academia(info json)
-    returns character varying
+create or replace function autorizacao_rfid_webservice_academia(info json) returns character varying
     language plpgsql
 as
 $$
@@ -11,6 +10,7 @@ DECLARE
     _equipamento      RECORD;
     _leitor           RECORD;
     _usuario          RECORD;
+    _monitoramento    RECORD;
     _monitor          RECORD;
     _alunos           RECORD;
     _registropersonal INT;
@@ -21,6 +21,7 @@ DECLARE
     _situacaoleitor   VARCHAR DEFAULT 'Autorizado';
     _dispositivo      VARCHAR DEFAULT 'Cartão de proximidade';
     _situacaopersonal BOOLEAN;
+    _situacaadolescente BOOLEAN;
 
 BEGIN
 
@@ -49,7 +50,6 @@ BEGIN
     WHERE autenticacao = _autenticacao;
     RAISE NOTICE 'Usuário: %', _usuario.nome;
 
-
     --Obtem informações do monitor de acesso ao local-----------------------------------------------------------------------
 
     SELECT INTO _monitor *
@@ -57,6 +57,8 @@ BEGIN
     WHERE autenticacao = _autenticacao
       AND equipamento = _equipamento.num;
     RAISE NOTICE 'Monitor do local(E=Entrada/S=Saida): %', _monitor.situacao;
+
+
 
     --Valida a entrada e a saída do usuário---------------------------------------------------------------------------------
 
@@ -119,6 +121,33 @@ BEGIN
         RAISE NOTICE 'O usuario encontra-se com status: %', _situacaolocal;
     END IF;
 
+    SELECT
+        INTO _monitoramento *
+    FROM acesso.autorizacao_rfid_webservice_monitoramento
+    WHERE autenticacao = _autenticacao;
+
+    if (_monitoramento.autenticacao notnull) then
+
+        RAISE NOTICE 'Registra o log de tentativa de passagem';
+        INSERT INTO acesso.passagem_pedestre (condominio, bloco, andar, unidade, autenticacao, bloqueio,
+                                              equipamento, localizacao_equipamento, leitor, localizacao_leitor,
+                                              situacao_leitor, situacao_usuario,
+                                              dsc_situacao, sentido, nome, tipo_de_cadastro, dispositivo)
+        VALUES (_monitoramento.condominio, _monitoramento.bloco, _monitoramento.andar, _monitoramento.unidade, _autenticacao, _monitoramento.bloqueio,
+                _equipamento.num, _equipamento.localizacao, _leitor.num, _leitor.localizacao, _situacaoleitor, 'Autorizado',
+                'Monitoramento', _leitor.sentido, _monitoramento.nome, 'Monitoramento', _dispositivo);
+
+        IF _monitor.situacao = 'E' THEN
+            _situacaolocal := 'S';
+        END IF;
+
+        RAISE NOTICE '#### SAIDA DO PROCEDIMENTO: O USUÁRIO É DO MONITORAMENTO E ESTÁ LIBERADO ####';
+        RETURN '{' || _monitoramento.nome || '|' || _situacaolocal || '}';
+
+    end if;
+
+
+
     --Valida o bloqueio operacional do usuário------------------------------------------------------------------------------
     IF _situacaopersonal = FALSE THEN -- Utilizado para validar o uso do chaveiro pelo personal
 
@@ -126,7 +155,7 @@ BEGIN
         _dscsituacao = 'O personal deve aguardar a passagem do chaveiro do morador';
         _situacaolocal = 'A';
         _situacaoleitor = 'Não autorizado';
-        RAISE NOTICE '  AGUARDANDO... ';
+        RAISE NOTICE '#### SAIDA DO PROCEDIMENTO: CHAVEIRO DO PERSONAL. ELE DEVE AGUARDAR A PASSAGEM DO CHAVEIRO DO MORADOR ####';
         RETURN NULL;
 
     END IF;
@@ -137,7 +166,7 @@ BEGIN
         _dscsituacao = 'Nenhum agendamento está confirmado para este registro';
         _situacaolocal = 'A';
         _situacaoleitor = 'Não autorizado';
-        RAISE NOTICE '  AGUARDANDO... ';
+        RAISE NOTICE '#### SAIDA DO PROCEDIMENTO: O HORÁRIO DE RESERVA AINDA NÃO ESTÁ DISPONÍVEL ####';
         RETURN NULL;
 
     END IF;
@@ -156,13 +185,34 @@ BEGIN
     RAISE NOTICE 'Valida a idade do usuário com a idade permitida no leitor';
     RAISE NOTICE 'A idade mínima para o leitor é % e o usuário possui % anos', _leitor.idade_minima, _usuario.idade;
 
-    IF (_usuario.nome NOTNULL) AND (_usuario.idade < _leitor.idade_minima) THEN
+    IF (_usuario.nome NOTNULL) AND (_usuario.idade < 12) THEN
 
         _autorizacao = 'Não autorizado';
         _dscsituacao = 'Idade não permitida';
         _situacaolocal = 'I';
-        RAISE NOTICE 'Idade não permitida';
-        RETURN '{null}';
+        RAISE NOTICE '#### SAIDA DO PROCEDIMENTO: A IDADE NÃO É PERMITIDA ####';
+        RETURN NULL;
+
+    ELSEIF (_usuario.nome NOTNULL) AND (_usuario.idade between 12 and 16) THEN
+
+        RAISE NOTICE 'NECESSÁRIO ACOMPANHANTE';
+        _situacaadolescente := acesso.alterna_status_adolescente(_autenticacao);
+        RAISE NOTICE 'SITUACAO DO CHAVEIRO %', _situacaadolescente;
+
+        IF (_situacaadolescente = FALSE) THEN
+            _autorizacao = 'Não autorizado';
+            _dscsituacao = 'O chaveiro do acompanhante ainda não autorizou';
+            _situacaolocal = 'I';
+            RAISE NOTICE '#### SAIDA DO PROCEDIMENTO: O CHAVEIRO DO ACOMPANHANTE AINDA NÃO AUTORIZOU A PASSAGEM ####';
+            RETURN NULL;
+        end if;
+
+    ELSEIF (_usuario.nome NOTNULL) THEN
+
+        UPDATE acesso.monitor_adolescente
+        SET situacao         = 'AUTORIZADO',
+            data_solicitacao = current_timestamp
+        WHERE unidade = _usuario.unidade_id;
 
     ELSEIF _usuario.nome ISNULL THEN
 
@@ -197,12 +247,18 @@ BEGIN
 
     --Define a saída da validação-------------------------------------------------------------------------------------------
 
-    RAISE NOTICE ' A situação do usuário é: %', _autorizacao;
+    RAISE NOTICE '#### SAIDA DO PROCEDIMENTO: FINAL DA VALIDAÇÃO O USUÁRIO ESTÁ % E O SENTIDO DA PASSAGEM É (%) ####', _autorizacao, _situacaolocal;
     RETURN '{' || _usuario.nome || '|' || _situacaolocal || '}';
 
-END ;
+END
 
 $$;
 
+alter function autorizacao_rfid_webservice_academia_excluir(json) owner to postgres;
 
-select acesso.autorizacao_rfid_webservice_academia('{"eq":"2488","li":"0","rfid":"1EBC5"}');
+select autorizacao_rfid_webservice_academia_excluir('{"eq":"2488","li":"0","rfid":"8E9B3"}');
+select autorizacao_rfid_webservice_academia_excluir('{"eq":"2488","li":"0","rfid":"8D7DD"}');
+
+select nome, autenticacao from condominio.moradores
+                                   join condominio.unidades on moradores.condominio = unidades.condominio and moradores.bloco = unidades.bloco and moradores.andar = unidades.andar and moradores.unidade = unidades.num
+where unidades.bloco = 7 and unidades.unidade = 33
